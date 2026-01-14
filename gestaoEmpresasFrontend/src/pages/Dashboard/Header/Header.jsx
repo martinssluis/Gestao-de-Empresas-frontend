797f95459684858c504e52b1a2c71dbf049263ff
Pedro Souza <pedrohenrique_2010@hotmail.com>
2026-01-13 22:08:23 -0300

    feat: Create Header Co-authored-by: Jerferson <jeefreitas315@gmail.com>

---
+++ gestaoEmpresasFrontend/src/pages/Dashboard/Header/Header.jsx
@@ -0,0 +1,24 @@
+import logo from '/logo.png';
+import styles from './Header.module.css';
+import Botao from './Botao.jsx';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className="LeftHeader">
+        <img
+          id={styles.logoSistema}
+          src={logo}
+          alt="Logo do sistema de gestão"
+        />
+      </div>
+
+      <nav aria-label="links_uteis">
+        <ul className={styles.list}>
+          <Botao idBotao={'botao1'} nome={'Dashboard'} />
+          <Botao idBotao={'botao2'} nome={'Estoque'} />
+        </ul>
+      </nav>
+    </header>
+  );
+}
