daecd6a2bc0e812a2c3b364bd5a8686a1b21ca68
Pedro Souza <pedrohenrique_2010@hotmail.com>
2026-01-29 22:03:42 -0300

    style: restyling the login page

--- gestaoEmpresasFrontend/src/pages/Login/Login.jsx
+++ gestaoEmpresasFrontend/src/pages/Login/Login.jsx
@@ -1,35 +1,44 @@
 import styles from './Login.module.css';
 import { useNavigate } from 'react-router-dom';
+import nomeSistema from '/nomeSistema.png';
+import imgLogin from '/imgLogin.png';
 
 export default function Login() {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.estiloLogin}>
-      <section>
-        <main className={styles.loginContainer}>
-          <h1>Login</h1>
-          <form className={styles.form}>
-            <input
-              className={styles.inputs}
-              type="text"
-              placeholder="Usuário"
-            />
-            <input
-              className={styles.inputs}
-              type="password"
-              placeholder="Senha"
-            />
-            <button
-              className={styles.buttons}
-              type="button"
-              onClick={() => navigate('/app/dashboard')}
-            >
-              Entrar
-            </button>
-          </form>
-        </main>
-      </section>
-    </div>
+    <section className={styles.loginPage}>
+      <div className={styles.divImagem}>
+        <img src={imgLogin} />
+      </div>
+      <div className={styles.estiloLogin}>
+        <section>
+          <main className={styles.loginContainer}>
+            <img className={styles.imgNomeSG} src={nomeSistema} />
+            <h2>Entre no SG</h2>
+            <p>usando seu usuário e senha</p>
+            <form className={styles.form}>
+              <input
+                className={styles.inputs}
+                type="text"
+                placeholder="Usuário"
+              />
+              <input
+                className={styles.inputs}
+                type="password"
+                placeholder="Senha"
+              />
+              <button
+                className={styles.buttons}
+                type="button"
+                onClick={() => navigate('/app/dashboard')}
+              >
+                Entrar
+              </button>
+            </form>
+          </main>
+        </section>
+      </div>
+    </section>
   );
 }
