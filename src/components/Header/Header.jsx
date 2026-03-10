93e47a49a599fca0ccb0e3a4b1098458df849ede
Pedro Souza <pedrohenrique_2010@hotmail.com>
2026-03-10 20:02:09 -0300

    style: reestyling header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,174 @@
-import logo from '/logo.png';
-import styles from './Header.module.css';
-import { useI18n } from '../../i18n/useI18n';
+// import styles from './Header.module.css';
+// import { useI18n } from '../../i18n/useI18n';
 
-export default function Header() {
-  const { t } = useI18n();
+// export default function Header() {
+//   const { t } = useI18n();
+
+//   return (
+//     <header className={styles.header}>
+//       <div className="LeftHeader">
+//         <img
+//           id={styles.logoSistema}
+//           src={logo}
+//           alt={t('header.logoAlt')}
+//         />
+//         {/* Criar funcionalidade do header icon account */}
+//       </div>
+//     </header>
+//   );
+// }
+
+import logoLight from '/logoLight.png';
+import logoDark from '/logoDark.png';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { useTheme } from '@mui/material/styles';
+
+const pages = [''];
+const settings = ['Meu perfil', 'Minha conta', 'Sair'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const theme = useTheme();
+  const isDark = theme.palette.mode === 'dark';
+  const currentLogo = isDark ? logoDark : logoLight;
 
   return (
-    <header className={styles.header}>
-      <div className="LeftHeader">
-        <img
-          id={styles.logoSistema}
-          src={logo}
-          alt={t('header.logoAlt')}
-        />
-        {/* Criar funcionalidade do header icon account */}
-      </div>
-    </header>
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: theme.palette.background.paper,
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            component="img"
+            src={currentLogo}
+            alt="Logo NexOrder"
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              height: 40,
+              mr: 1,
+            }}
+          />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            component="img"
+            src={currentLogo}
+            alt="Logo"
+            sx={{
+              display: { xs: 'flex', md: 'none' },
+              height: 35,
+              mr: 1,
+            }}
+          />
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Abrir configurações">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Mohamed Lee" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
+export default ResponsiveAppBar;
